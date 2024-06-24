db558091ed7da35e475ce021a67c5ffa8450c664
singhalaryann <aryan.singhal.20cse@bmu.edu.in>
2024-06-24 13:19:39 +0530

    Changes

--- App.js
+++ App.js
@@ -4,13 +4,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Episode from './screens/Episode';
 import Page1 from './screens/Page1';
-import MainScreen from './screens/Mainscreen';
+// import MainScreen from './screens/MainScreen';
 import SignupPage from "./screens/Signup";
 import Signin from "./screens/Signin";
 import Homepage from "./screens/Homepage";
 import Levels from "./screens/Levels";
 import Help from './screens/Help';
-import Splash from './screens/Splash';
 import Setting from './screens/Setting';
 import { Audio } from 'expo-av'; // Import Audio from expo-av
 
@@ -42,15 +41,14 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash" screenOptions={{ presentation: 'transparentModal' }}>
-        <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
-        <Stack.Screen name="MainScreen" component={MainScreen} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName="Homepage" screenOptions={{ presentation: 'transparentModal' }}>
+        <Stack.Screen name="Homepage" component={Homepage} options={{ headerShown: false, gestureEnabled: false }} />
+        {/* <Stack.Screen name="MainScreen" component={MainScreen} options={{ headerShown: false }} /> */}
         <Stack.Screen name="SignupPage" component={SignupPage} options={{ headerShown: false }} />
         <Stack.Screen name="SigninPage" component={Signin} options={{ headerShown: false }} />
         <Stack.Screen name="Ep1" component={Episode} options={{ headerShown: false }} />
         <Stack.Screen name="Setting" component={Setting} options={{ headerShown: false }} />
         <Stack.Screen name="Page1" component={Page1} options={{ headerShown: false }} />
-        <Stack.Screen name="HomePage" component={Homepage} options={{ headerShown: false, gestureEnabled: false }} />
         <Stack.Screen name="LevelsPage" component={Levels} options={{ headerShown: false }} />
         <Stack.Screen name="HelpPage" component={Help} options={{ headerShown: false }} />
       </Stack.Navigator>
@@ -59,4 +57,3 @@ const App = () => {
 };
 
 export default App;
-  
